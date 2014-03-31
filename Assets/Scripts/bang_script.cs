4373cea7f9c43073ec6ccaa2fde975fb8b714c98
unknown <rmarch03@villanova.edu>
2014-03-31 16:23:51 -0400

    The Bang Now Appears

---
+++ Assets/Scripts/bang_script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bang_script : MonoBehaviour {
+
+    public GameObject bang;
+    public Vector3 spawn_position;
+    float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > 5)
+        {
+            Instantiate(bang, spawn_position, Quaternion.identity);
+        }
+	}
+}
